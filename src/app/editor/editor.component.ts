4f52a3c15be49bc519dfabaa01f8cc19dd7b228b
Rohan Ramhari Jadhav <jadhavrohan5050@gmail.com>
2023-08-25 15:57:30 +0530

    dynamically added dialog box position

--- src/app/editor/editor.component.ts
+++ src/app/editor/editor.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, ElementRef, OnInit, Renderer2 } from "@angular/core";
 import { FormBuilder, FormControl, FormGroup } from "@angular/forms";
 import { AppService } from "../app.service";
 import { faArrowRightLong } from '@fortawesome/free-solid-svg-icons'
@@ -10,16 +10,21 @@ import { faArrowRightLong } from '@fortawesome/free-solid-svg-icons'
 })
 export class EditorComponent implements OnInit {
     faArrowRightLong = faArrowRightLong;
-
+    boxPosition: { 'left': string, 'top': string } = {
+        left: '0px',
+        top: '0px'
+    }
     CurrentIdeas = new FormGroup({})
-    ideaMenuOpen: {'flag': boolean, 'control': string} = {
+    ideaMenuOpen: { 'flag': boolean, 'control': string } = {
         'flag': false,
         'control': ''
     }
-
     allIdeas = [['idea2', 'idea1', 'idea0'], ['idea5', 'idea4', 'idea3', 'idea2']]
-    constructor(private appService: AppService, private formBuilder: FormBuilder) {
-    }
+
+    constructor(
+        private appService: AppService,
+        private formBuilder: FormBuilder
+    ) {}
 
     ngOnInit(): void {
         this.CurrentIdeas = this.formBuilder.group({});
@@ -34,12 +39,9 @@ export class EditorComponent implements OnInit {
         })
     }
 
-    changeDetection(event: any,controlName: string) {
-        const eventRef = event.target as HTMLTextAreaElement;
-        let selectionStart = eventRef.selectionStart;
+    changeDetection(event: any, controlName: string) {
         let controlValue = this.CurrentIdeas.get(controlName)?.value;
-        let modifiedValue = '';
-        if(controlValue.endsWith('<>')) {
+        if (controlValue.endsWith('<>')) {
             this.ideaMenuOpen = {
                 flag: true,
                 control: controlName
@@ -47,19 +49,22 @@ export class EditorComponent implements OnInit {
         }
         else {
             this.ideaMenuOpen = {
-                flag : false,
+                flag: false,
                 control: ''
             }
         }
+        this.boxPosition = this.getCursorPos(event, controlName);
+
     }
 
-    addAnotherIdea(controlName: string, index: number) {
+
+    addAnotherIdeaAsRef(controlName: string, index: number) {
         let controlValue = this.CurrentIdeas.get(controlName)?.value;
         let modifiedValue = '';
-        if(controlValue.endsWith('<>')) {
-           modifiedValue = controlValue.slice(0, -2);
-           modifiedValue += `<span class="hightlight"><> ${this.convertAllIdeas(this.allIdeas[index])}</span>`
-           console.log(modifiedValue)
+        if (controlValue.endsWith('<>')) {
+            modifiedValue = controlValue.slice(0, -2);
+            modifiedValue += `<span class="hightlight"><> ${this.convertAllIdeas(this.allIdeas[index])}</span>`
+            console.log(modifiedValue)
         }
 
         console.log(modifiedValue)
@@ -71,15 +76,38 @@ export class EditorComponent implements OnInit {
 
     private convertAllIdeas(value: string[]): string {
         let str = `<span class="highlight"><></span>`
-        value.forEach((val, i)=> {
+        value.forEach((val, i) => {
             str += `
             <span class="highlight">${val}</span>
             `
-            if(i < value.length) {
+            if (i < value.length) {
                 str += `<fa-icon class="arrow-right" [icon]="faArrowRightLong"></fa-icon>`
             }
         })
 
         return str;
+    }
+
+    private getCursorPos(event: any, controlName: string): { 'left': string, 'top': string } {
+        const eventRef = event.target as HTMLTextAreaElement;
+        let selectionStart = eventRef.selectionStart;
+        const textBeforeCaret = eventRef.value.substring(0, selectionStart);
+        const span = document.createElement('span');
+        span.style.visibility = 'hidden';
+        span.style.position = 'absolute';
+        span.style.whiteSpace = 'pre-wrap';
+        span.style.font = window.getComputedStyle(eventRef).font;
+        span.textContent = textBeforeCaret;
+        document.body.appendChild(span);
+        const caretPositionX = span.offsetWidth;
+        const caretPositionY = span.offsetHeight;
+        console.log('offsetx ', span.offsetTop)
+        document.body.removeChild(span);
+        console.log(`Caret Position X: ${caretPositionX}px / ${caretPositionY}px.`);
+
+        return {
+            'left': `${caretPositionX}px`,
+            'top': `${caretPositionY + 50}px`
+        }
     }
 }
